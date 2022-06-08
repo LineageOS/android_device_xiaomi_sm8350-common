52532c88f434a9b6c465268eea0ec026307ea15e
Alexander Koskovich <akoskovich@pm.me>
2022-06-08 12:59:31 -0700

    sm8350-common: Define boot image security patch
    Change-Id: I2471d3412577fee8e7bca40743b38a2fc765884c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -154,7 +154,8 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-09-01
+BOOT_SECURITY_PATCH := 2025-09-01
+VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
