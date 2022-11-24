1e86fc3a26ba16757f1b97d931d9fac311f6cc8b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-24 23:30:29 +0100

    sm8350-common: Drop SDM LiveDisplay service
    * We switched to AOSP colors so messing with color calibration will
      confuse QDCM
    
    Change-Id: Ia22c4c06e922d1b7f9c60b257299634333cc3fe4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,7 +69,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/hidl/manifest_lahaina.xml \
-    $(COMMON_PATH)/hidl/manifest_lineage.xml \
     $(COMMON_PATH)/hidl/manifest_xiaomi.xml
 
 # Kernel
