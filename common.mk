23a8cf25987efcefd63f73e5220ee30ccbf3d55b
Giovanni Ricca <giovanniricca@proton.me>
2025-06-09 22:19:31 +0200

    sm8350-common: Opt-in libudfpshandler
    Change-Id: If67c415d33215d9ef954ded4d118aad5841e589a

--- common.mk
+++ common.mk
@@ -153,6 +153,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi
 
+ifeq ($(TARGET_HAS_UDFPS),true)
+PRODUCT_PACKAGES += \
+    libudfpshandler
+endif
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
