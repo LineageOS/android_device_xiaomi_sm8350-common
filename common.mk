6a738e2bc0209f51c7aaedcdf81cf16ab6cb975d
Giovanni Ricca <giovanniricca@proton.me>
2025-06-10 19:51:34 +0200

    sm8350-common: rro_overlays: Simplify RROs usage
    Change-Id: If906cb6f0d90872c8ceeea147516b408ec5cf215

--- common.mk
+++ common.mk
@@ -315,6 +315,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlaySM8350 \
+    WifiOverlaySM8350
+
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
@@ -357,10 +361,6 @@ PRODUCT_COPY_FILES += \
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
-
-# RIL
-PRODUCT_PACKAGES += \
-    CarrierConfigOverlay
 
 # Sensors
 PRODUCT_PACKAGES += \
@@ -462,7 +462,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
     libwifi-hal-qcom \
-    WifiOverlay \
     wpa_cli \
     wpa_supplicant \
     wpa_supplicant.conf
