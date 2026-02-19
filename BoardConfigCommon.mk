768f74244122c5c794cbf6a287f25e3a4047b4d4
Giovanni Ricca <giovanniricca@proton.me>
2026-02-19 21:01:05 +0100

    sm8350-common: Update blobs to redwood OS2.0.17.0.UMSMIXM
    Change-Id: I5e5dba6cd2cd5d41da774e22c8e2844a0c54271a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -156,7 +156,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-09-01
+VENDOR_SECURITY_PATCH := 2025-11-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
